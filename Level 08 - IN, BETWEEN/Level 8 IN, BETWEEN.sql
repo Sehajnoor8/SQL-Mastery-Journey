4afacb0710e704023ec726b5d7b554e22a097452
Sehajnoor8 <sehajn8@gmail.com>
2026-06-10 21:30:01 +0530

    Update Level 8 IN, BETWEEN.sql

--- Level 08 - IN, BETWEEN/Level 8 IN, BETWEEN.sql
+++ Level 08 - IN, BETWEEN/Level 8 IN, BETWEEN.sql
@@ -1,43 +1,57 @@
-USE linkedin_job_analytics;
-SELECT
-    company_name,
-    COUNT(job_id) AS total_openings
-FROM companies
-JOIN jobs
-ON companies.company_id = jobs.company_id
-GROUP BY company_name
-ORDER BY total_openings DESC;
+USE practice;
 
-SELECT
-    company_name,
-    AVG(max_salary) AS avg_salary
-FROM companies
-JOIN jobs
-ON companies.company_id = jobs.company_id
-JOIN salaries
-ON jobs.job_id = salaries.job_id
-GROUP BY company_name
-ORDER BY avg_salary DESC;
+# Level 08 – IN & BETWEEN
 
-SELECT
-    skill_name,
-    COUNT(job_id) AS demand_count
-FROM skills
-JOIN job_skills
-ON skills.skill_id = job_skills.skill_id
-GROUP BY skill_name
-ORDER BY demand_count DESC;
+# IN
 
-SELECT
-    remote_option,
-    COUNT(*) AS total_jobs
-FROM locations
-JOIN jobs
-ON locations.location_id = jobs.location_id
-GROUP BY remote_option;
+# 76. Find employees from Delhi, Mumbai, or Pune.
+SELECT *
+FROM employees
+WHERE city IN ('Delhi', 'Mumbai', 'Pune');
 
-SELECT
-    experience_level,
-    COUNT(*) AS total_jobs
-FROM jobs
-GROUP BY experience_level;
+# 77. Find employees in IT or HR departments.
+SELECT *
+FROM employees
+WHERE department IN ('IT', 'HR');
+
+# 78. Find employees whose salary is 40000, 50000, or 65000.
+SELECT *
+FROM employees
+WHERE salary IN (40000, 50000, 65000);
+
+# 79. Find employees from Bangalore or Delhi.
+SELECT *
+FROM employees
+WHERE city IN ('Bangalore', 'Delhi');
+
+# 80. Find employees in Finance or Marketing.
+SELECT *
+FROM employees
+WHERE department IN ('Finance', 'Marketing');
+
+# BETWEEN
+
+# 81. Find employees with salary between 45000 and 60000.
+SELECT *
+FROM employees
+WHERE salary BETWEEN 45000 AND 60000;
+
+# 82. Find employees with salary between 50000 and 70000.
+SELECT *
+FROM employees
+WHERE salary BETWEEN 50000 AND 70000;
+
+# 83. Find employees who joined between '2022-01-01' and '2024-12-31'.
+SELECT *
+FROM employees
+WHERE joining_date BETWEEN '2022-01-01' AND '2024-12-31';
+
+# 84. Find employees whose salary is between 40000 and 50000.
+SELECT *
+FROM employees
+WHERE salary BETWEEN 40000 AND 50000;
+
+# 85. Find employees whose salary is not between 45000 and 60000.
+SELECT *
+FROM employees
+WHERE salary NOT BETWEEN 45000 AND 60000;
